01e97a02e19db4bbf6310b752d3a94b929276191
mwaniasam <s.mwania@alustudent.com>
2025-01-12 09:30:28 -0800

    Reloaded objects. Created a new object!

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,8 +13,8 @@ class FileStorage:
         """
         Sets in __objects the obj with key <obj class name>.id
         """
-        obj_class_name = obj.__class__.__name__
-        key = "{}.{}".format(obj_class_name, obj.id)
+        obj_cls_name = obj.__class__.__name__
+        key = "{}.{}".format(obj_cls_name, obj.id)
         FileStorage.__objects[key] = obj
 
     def all(self):
@@ -27,28 +27,31 @@ class FileStorage:
         """
         Serializes __objects to the JSON file (path: __file_path)
         """
-        all_obj = FileStorage.__objects
+        all_objs = FileStorage.__objects
         obj_dict = {}
-        for key in all_obj.keys():
-            obj_dict[key] = all_obj[key].to_dict()
 
-        with open(FileStorage.__file_path, 'w') as f:
+        for obj in all_objs.keys():
+            obj_dict[obj] = all_objs[obj].to_dict()
+
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         """
         Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
         """
-        if os.path.exists(FileStorage.__file_path):
+        if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r', encoding="utf-8") as f:
                 try:
                     obj_dict = json.load(f)
+
                     for key, value in obj_dict.items():
                         class_name, obj_id = key.split('.')
-                        class_name = eval(class_name)
-
-                        instance = class_name(**value)
+                        class_ = eval(class_name)
+                        
+                        instance = class_(**value)
+                        instance.id = obj_id
                         FileStorage.__objects[key] = instance
 
-                except json.JSONDecodeError:
+                except Exception:
                     pass
---
+++ tests/test_models/test_engine/test_base_model.py
@@ -0,0 +1,55 @@
+import os
+import unittest
+import models
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+class TestFileStorageInstantiation(unittest.TestCase):
+    """Testing the instantiation of file storage."""
+
+    def test_FileStorage_instantiation_no_args(self):
+        """Test creating a FileStorage instance with no arguments."""
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+    def test_FileStorage_instantiation_with_arg(self):
+        """Test creating a FileStorage instance with an argument
+        (should raise TypeError)."""
+        with self.assertRaises(TypeError):
+            FileStorage(None)
+
+    def test_storage_initializes(self):
+        """Test if the storage variable in models is an instance of FileStorage."""
+        self.assertEqual(type(models.storage), FileStorage)
+
+class TestFileStorage(unittest.TestCase):
+
+    def setUp(self):
+        """Create a temporary test file for saving data."""
+        self.test_file = "test_file.json"
+
+    def tearDown(self):
+        """Remove the temporary test file after the test."""
+        if os.path.exists(self.test_file):
+            os.remove(self.test_file)
+
+    def test_all_storage_returns_dictionary(self):
+        """Test if the all() method returns a dictionary."""
+        self.assertEqual(dict, type(models.storage.all()))
+
+    def test_new(self):
+        """Test the new method by creating and storing a new object."""
+        obj = BaseModel()
+        models.storage.new(obj)
+        self.assertIn("BaseModel.{}".format(obj.id), models.storage.all())
+
+    def test_new_with_args(self):
+        """Test creating a new object with additional arguments
+        (should raise TypeError)."""
+        with self.assertRaises(TypeError):
+            models.storage.new(BaseModel(), 1)
+
+    def test_new_with_None(self):
+        """Test creating a new object with None as argument
+        (should raise AttributeError)."""
+        with self.assertRaises(AttributeError):
+            models.storage.new(None)
