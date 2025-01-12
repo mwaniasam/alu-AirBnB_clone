7c6ae14c6261ee4ebaf059efce2fe8b93d4fc870
mwaniasam <s.mwania@alustudent.com>
2025-01-12 07:18:36 -0800

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,22 @@ class BaseModel:
     """
     Base model class for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        time_format = '%Y-%m-%dT%H:%M:%S.%f'
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
         self.id = str(uuid.uuid4())
 
         self.created_at = datetime.now()
