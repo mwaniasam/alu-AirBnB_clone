9f995e1da938006ac26569dca3c4ac14257f30e8
mwaniasam <s.mwania@alustudent.com>
2025-01-12 07:25:34 -0800

    Remove repetition

--- models/base_model.py
+++ models/base_model.py
@@ -28,11 +28,6 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
-        self.id = str(uuid.uuid4())
-
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
     def save(self):
         """
         Updates the public instance attribute updated_at with the current datetime
