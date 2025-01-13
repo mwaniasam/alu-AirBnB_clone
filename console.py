f28226e488458c5dccf2681a795d1f666102b24d
mwaniasam <s.mwania@alustudent.com>
2025-01-12 22:05:05 -0800

    Document the module

--- console.py
+++ console.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
-"""
 
 """
+HBNB command-line interpreter module.
+
+This module provides a simple command-line interpreter for HBNB.
+"""
+
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """HBNB command-line interpreter"""
-    prompt = "(hbnb) "
+    """
+    HBNB command-line interpreter
+    """
+    prompt = "(hbnb)"
 
     def do_quit(self, arg):
-        """Quit command to exit the program"""
+        """
+        Quit command to exit the program
+        """
         return True
 
     def help_quit(self):
-        """Help for quit command"""
+        """
+        Help for quit command
+        """
         print("Quit command to exit the program")
 
     def do_EOF(self, arg):
-        """Exit the program on EOF (Ctrl+D)"""
+        """
+        Exit the program on EOF (Ctrl+D)
+        """
         print()
         return True
 
