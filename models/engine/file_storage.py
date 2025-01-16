8fc383bdcea29eab8f96e89ac44919178300ee6e
mwaniasam <s.mwania@alustudent.com>
2025-01-16 02:41:45 -0800

    Update FileStorage to manage correctly serialization and deserialization of all our new classes: Place, State, City, Amenity and Review

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,11 @@ import json
 import os
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class FileStorage:
     """
@@ -39,20 +44,30 @@ class FileStorage:
 
     def reload(self):
         """
-        Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
+        Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists; otherwise, do nothing)
         """
         if os.path.isfile(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r', encoding="utf-8") as f:
                 try:
                     obj_dict = json.load(f)
+                    class_map = {
+                        "BaseModel": BaseModel,
+                        "User": User,
+                        "State": State,
+                        "City": City,
+                        "Amenity": Amenity,
+                        "Place": Place,
+                        "Review": Review
+                    }
 
                     for key, value in obj_dict.items():
                         class_name, obj_id = key.split('.')
-                        class_ = eval(class_name)
-                        
-                        instance = class_(**value)
-                        instance.id = obj_id
-                        FileStorage.__objects[key] = instance
+                        class_ = class_map.get(class_name)
+
+                        if class_:
+                            instance = class_(**value)
+                            instance.id = obj_id
+                            FileStorage.__objects[key] = instance
 
                 except Exception:
                     pass
